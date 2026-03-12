782242a93e441d255e4f76c4553a27124d215048
Chaitanya Sethi <sethichaitanya08@gmail.com>
2026-03-13 03:52:52 +0530

    05 Dashboard part2

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar"
+import { getallplaygroundsofUser } from "@/features/dashboard/actions"
 import { DashboardSidebar } from "@/features/dashboard/components/dashboard-sidebar"
 
 import type React from "react"
@@ -8,31 +9,31 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode
 }) {
-//   const playgroundData = await getAllPlaygroundForUser()
+  const playgroundData = await getallplaygroundsofUser()
 
-//   // Store icon names (strings) instead of the components themselves
-//   const technologyIconMap: Record<string, string> = {
-//     REACT: "Zap",
-//     NEXTJS: "Lightbulb",
-//     EXPRESS: "Database",
-//     VUE: "Compass",
-//     HONO: "FlameIcon",
-//     ANGULAR: "Terminal",
-//   }
+  // Store icon names (strings) instead of the components themselves
+  const technologyIconMap: Record<string, string> = {
+    REACT: "Zap",
+    NEXTJS: "Lightbulb",
+    EXPRESS: "Database",
+    VUE: "Compass",
+    HONO: "FlameIcon",
+    ANGULAR: "Terminal",
+  }
 
-//   const formattedPlaygroundData =
-//     playgroundData?.map((item) => ({
-//       id: item.id,
-//       name: item.title,
-//       starred: item.Starmark?.[0]?.isMarked || false,
-//       // Pass the icon name as a string
-//       icon: technologyIconMap[item.template] || "Code2", // Default to "Code2" if template not found
-//     })) || []
+  const formattedPlaygroundData =
+    playgroundData?.map((item) => ({
+      id: item.id,
+      name: item.title,
+      starred: item.Starmark?.[0]?.ismarked || false,
+      // Pass the icon name as a string
+      icon: technologyIconMap[item.template] || "Code2", // Default to "Code2" if template not found
+    })) || []
 
   return (
     <SidebarProvider>
       <div className="flex min-h-screen w-full overflow-x-hidden">
-        <DashboardSidebar initialPlaygroundData={[]} />
+        <DashboardSidebar initialPlaygroundData={formattedPlaygroundData} />
         <main className="flex-1">{children}</main>
       </div>
     </SidebarProvider>
---
+++ features/dashboard/actions/index.ts
@@ -0,0 +1,110 @@
+"use server";
+import { currentUser } from "@/features/auth/actions";
+import { db } from "@/lib/db";
+// @ts-ignore
+import {Templates} from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export const createplayground = async(data:{title:string;template:Templates;
+    description:string;
+    userId:string;
+})=>{
+    const {title,template,description}= data;
+    const user = await currentUser();
+    try {
+    const playground =     await db.playground.create({
+            data:{
+                title,
+                template,
+                description,
+                userId:user?.id || ""
+            }
+        });
+        return playground;
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}  
+
+
+export const getallplaygroundsofUser = async()=>{
+    const user = await currentUser();
+    try {
+        const playgrounds = await db.playground.findMany({
+            where:{
+                userId:user?.id
+            },
+            include:{
+                user:true,
+                Starmark:{
+                    where:{
+                        userId:user?.id 
+                    },
+                    select:{
+                        ismarked:true,
+                    }
+                }
+            }
+        });
+        return playgrounds;
+    } catch (error) {
+            console.log(error);
+            return null;
+    }
+}
+
+export const deleteprojectbyid = async(id:string)=>{
+    try {
+            await db.playground.delete({
+                where:{
+                    id
+                }
+            }); 
+            revalidatePath("/dashboard");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+export const Editprojectbyid = async(id:string,data:{title?:string;description?:string})=>{
+    try {
+        await db.playground.update({
+            where:{
+                id
+            },
+           data:data
+        });
+        revalidatePath("/dashboard");       
+    } catch (error) {
+        
+    }
+}
+
+export const duplicateprojectbyid = async(id:string)=>{
+    try{
+            const orignalplayground = await db.playground.findUnique({
+                where:{
+                    id
+                }
+            });
+            if(!orignalplayground){throw new Error("Original playground not found");}
+            const duplicatedplayground = await db.playground.create({
+                data:{
+                    title:`${orignalplayground.title} (Copy)`,
+                    description:orignalplayground.description,
+                    template:orignalplayground.template,
+                    userId:orignalplayground.userId
+                }
+            });
+            revalidatePath("/dashboard");   
+            return duplicatedplayground;
+           
+    }catch(error){
+        console.log(error);
+        return null;
+    }
+}
